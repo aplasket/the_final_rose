7c7322223fe9fe7df157680e1f6b27edd694f363
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-11 12:35:46 -0400

    add contestants index action route view

---
+++ spec/features/contestants/index_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "bachlorettes/:id/contestants, contestant index page", type: :feature do
+  describe "as a visitor on the contestant index page" do
+    it "shows all contestants for that bachelorette" do
+      
+    end
+  end
+end
